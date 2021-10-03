a67bf42d8846c7242a8a36a620f1257e718249d5
Laura Shi <bmsmjaa@gmail.com>
2021-10-03 23:00:43 +0800

    completes function enroll

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -49,18 +49,16 @@ contract SimpleBank {
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
     function () external payable {
-        if (msg.value > 0) {
-          emit LogDepositMade(msg.sender, msg.value);
-	      }
         revert();
     }
 
     /// @notice Get balance
     /// @return The balance of the user
-    function getBalance() public returns (uint) {
+    function getBalance() public view returns (uint) {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -69,27 +67,23 @@ contract SimpleBank {
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
       enrolled[msg.sender] = true;
-
       emit LogEnrolled(msg.sender);
-
       return enrolled[msg.sender];
-  
-    
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
-    
       // 2. Users should be enrolled before they can make deposits
-
+      require(enrolled[msg.sender] == true);
       // 3. Add the amount to the user's balance. Hint: the amount can be
       //    accessed from of the global variable `msg`
-
+      balances[msg.sender] += msg.value;
       // 4. Emit the appropriate event associated with this function
-
+      emit LogDepositMade(msg.sender, balances[msg.sender]);
       // 5. return the balance of sndr of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -103,10 +97,12 @@ contract SimpleBank {
       // return the user's balance.
 
       // 1. Use a require expression to guard/ensure sender has enough funds
-
+      require(balances[msg.sender] >= withdrawAmount);
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
-
+      balances[msg.sender] -= withdrawAmount;
       // 3. Emit the appropriate event for this message
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+      return balances[msg.sender];
     }
 }
